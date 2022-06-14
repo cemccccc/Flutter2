2f805192cdce8e7846538fc3dd15cfa2cf2da43a
cemyasar <cemyasaroguz@gmail.com>
2022-06-15 00:42:25 +0300

    textwidgetCommit

--- lib/101/text_learn_view.dart
+++ lib/101/text_learn_view.dart
@@ -1,16 +1,56 @@
 import 'package:flutter/material.dart';
 
 class TextLearnView extends StatelessWidget {
-  const TextLearnView({Key? key}) : super(key: key);
+  const TextLearnView({Key? key, this.userName}) : super(key: key);
+
+  final String name = 'Cem';
+
+  final String? userName;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          'Cem',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //column center olur
+          children: [
+            Text(
+              'Welcome $name ${name.length}',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.right,
+              style: ProjectStyles.welcomeStyle,
+            ),
+            Text(
+              'Welcome $name ${name.length}',
+              maxLines: 2, // satırlar arası boşluk
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.right, // text yaslama
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  ?.copyWith(color: ProjectColors.welcomeColor),
+            ),
+            Text(userName ?? ' '), //null check
+          ],
         ),
       ),
     );
   }
+}
+
+class ProjectStyles {
+  static TextStyle welcomeStyle = const TextStyle(
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.italic,
+      height: 1.5,
+      color: Colors.lime,
+      wordSpacing: 2,
+      letterSpacing: 2,
+      fontSize: 16,
+      fontWeight: FontWeight.w600);
+}
+
+class ProjectColors {
+  static Color welcomeColor = Colors.yellow;
 }
