31bb5267aeddb57a36dacae3ebbc49e8763444c9
cemyasar <cemyasaroguz@gmail.com>
2022-07-13 23:22:57 +0300

    test

---
+++ lib/202/model_learn_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ModelLearnView extends StatefulWidget {
+  ModelLearnView({Key? key}) : super(key: key);
+
+  @override
+  State<ModelLearnView> createState() => _ModelLearnViewState();
+}
+
+class _ModelLearnViewState extends State<ModelLearnView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
